1f2c65ba7ab0bbd3c8ea8c2d24bbddf4a35a1457
Seth Tal <seth.tal255@gmail.com>
2021-01-18 11:10:09 -0800

    minor change

---
+++ py-module-testing/testSKLearn.py
@@ -0,0 +1,14 @@
+import sklearn as skl
+import pandas as p
+import matplotlib as mpl
+
+
+SAVEFILEPATH = "C:\\Users\\Stal\\tree_saves"
+
+
+def main():
+    print(SAVEFILEPATH)
+
+
+if __name__ == "__main__":
+    main()
