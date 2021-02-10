5298cdfe5abf29652d94c4ca94e8df913d1b54e3
Seth Tal <seth.tal255@gmail.com>
2021-02-10 13:05:45 -0800

    minor pep8 violation

--- setup.py
+++ setup.py
@@ -12,11 +12,13 @@ setup(
     name="transformation-tree",
     version="1.1.0",
     python_requires=">=3.7.9",
-    description="Transformation Tree library developed for CIS 422 @ University of Oregon",
+    description="Transformation Tree library developed for CIS 422 @ \
+                 University of Oregon",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Sephta/time-series-modeling",
-    author="Seth Tal, Alec Springel, Ronny Fuentes, Stephanie Schofield, Kyra Novitzky",
+    author="Seth Tal, Alec Springel, Ronny Fuentes, Stephanie Schofield, Kyra \
+            Novitzky",
     packages=find_packages(exclude=("tests")),
     include_package_data=True,
     install_requires=["scikit-learn",
